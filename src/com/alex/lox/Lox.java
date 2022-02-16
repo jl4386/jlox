170c84d7abf59635bfbae8f5c094f0e655e79c90
giddens9527 <jl4386@outlook.com>
2022-02-16 17:06:07 +0800

    feat: parser finished

--- src/com/alex/lox/Lox.java
+++ src/com/alex/lox/Lox.java
@@ -39,10 +39,13 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
 
-    for (Token token: tokens) {
-      System.out.println(token);
-    }
+    // Stop if there was a syntax error
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
   }
   static void error(int line, String message) {
     report(line, "", message);
@@ -50,5 +53,12 @@ public class Lox {
   private static void report(int line, String where, String message) {
     System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 }
