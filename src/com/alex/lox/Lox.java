f0aa4541bb1264032bbc425505b5d1e80a06dd54
giddens9527 <jl4386@outlook.com>
2022-02-18 14:15:52 +0800

    feat: interpreter for simple calculator

--- src/com/alex/lox/Lox.java
+++ src/com/alex/lox/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
+  
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -24,6 +27,7 @@ public class Lox {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
   private static void runPrompt() throws IOException {
     InputStreamReader input = new InputStreamReader(System.in);
@@ -44,11 +48,14 @@ public class Lox {
 
     // Stop if there was a syntax error
     if (hadError) return;
-
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
   static void error(int line, String message) {
     report(line, "", message);
+  }
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
   private static void report(int line, String where, String message) {
     System.err.println("[line " + line + "] Error" + where + ": " + message);
