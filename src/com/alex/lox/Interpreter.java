46d86f8b9182ac65fc5d17f6dc882ee8596b2f74
giddens9527 <jl4386@outlook.com>
2022-02-27 18:30:26 +0800

    feat: for function call

--- src/com/alex/lox/Interpreter.java
+++ src/com/alex/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.alex.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -36,6 +37,18 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return !isTruthy(right);
     }
     return null;
+  }
+
+  @Override
+  public Object visitCallExpr(Expr.Call expr) {
+    Object callee = evaluate(expr.callee);
+
+    List<Object> arguments = new ArrayList<>();
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+    LoxCallable function = (LoxCallable) callee;
+    return function.call(this, arguments);
   }
 
   @Override
