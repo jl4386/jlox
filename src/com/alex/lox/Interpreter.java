29673114560cf340d65850d4334b73d79fe65db4
giddens9527 <jl4386@outlook.com>
2022-03-01 16:46:30 +0800

    feat: for native/foreign functions

--- src/com/alex/lox/Interpreter.java
+++ src/com/alex/lox/Interpreter.java
@@ -4,7 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-  private Environment environment = new Environment();
+
+  final Environment globals = new Environment();
+  private Environment environment = globals;
+
+  Interpreter() {
+    // Anonymous class
+    // new InterfaceName() {}
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        return (double) System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() {
+        return "<native fn>";
+      }
+    });
+  }
 
   void interpret(List<Stmt> statements) {
     try {
@@ -47,7 +70,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     for (Expr argument : expr.arguments) {
       arguments.add(evaluate(argument));
     }
+    if (!(callee instanceof LoxCallable)) {
+      throw new RuntimeError(expr.paren, "Can only call functions and classes");
+    }
     LoxCallable function = (LoxCallable) callee;
+    if (arguments.size() != function.arity()) {
+      throw new RuntimeError(expr.paren, "Expected " + function.arity() + " arguments but got " + arguments.size() + ".");
+    }
     return function.call(this, arguments);
   }
 
