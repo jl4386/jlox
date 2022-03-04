ac1570f09c6e40bcf29622919f4f47d1080a228d
giddens9527 <jl4386@outlook.com>
2022-03-04 18:02:01 +0800

    feat: basic resolver class - part 2 resolve

--- src/com/alex/lox/Resolver.java
+++ src/com/alex/lox/Resolver.java
@@ -6,4 +6,26 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   Resolver(Interpreter interpreter) {
     this.interpreter = interpreter;
   }
+
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    beginScope();
+    resolve(stmt.statements);
+    endScope();
+    return null;
+  }
+
+  private void resolve(Stmt stmt) {
+    stmt.accept(this);
+  }
+
+  private void resolve(Expr expr) {
+    expr.accept(this);
+  }
+
+  void resolve(List<Stmt> statements) {
+    for (Stmt statement : statements) {
+      resolve(statement);
+    }
+  }
 }
