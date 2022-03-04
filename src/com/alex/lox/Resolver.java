c728e01368dd765e2eef87d9a5ee57fa787af8c0
giddens9527 <jl4386@outlook.com>
2022-03-04 17:50:58 +0800

    feat: basic resolver class

---
+++ src/com/alex/lox/Resolver.java
@@ -0,0 +1,9 @@
+package com.alex.lox;
+
+public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+  private final Interpreter interpreter;
+
+  Resolver(Interpreter interpreter) {
+    this.interpreter = interpreter;
+  }
+}
