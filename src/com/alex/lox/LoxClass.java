ca5d843f11b81fb46c078b086e21aee6bbb95e69
giddens9527 <jl4386@outlook.com>
2022-03-06 17:27:00 +0800

    feat: class declaration

---
+++ src/com/alex/lox/LoxClass.java
@@ -0,0 +1,14 @@
+package com.alex.lox;
+
+public class LoxClass {
+  final String name;
+
+  LoxClass(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+}
--- src/com/alex/lox/Stmt.java
+++ src/com/alex/lox/Stmt.java
@@ -6,21 +6,16 @@ abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
 
+    R visitClassStmt(Class stmt);
+
     R visitExpressionStmt(Expression stmt);
-
     R visitFunctionStmt(Function stmt);
-
     R visitIfStmt(If stmt);
-
     R visitPrintStmt(Print stmt);
-
     R visitReturnStmt(Return stmt);
-
     R visitVarStmt(Var stmt);
-
     R visitWhileStmt(While stmt);
   }
-
   static class Block extends Stmt {
     Block(List<Stmt> statements) {
       this.statements = statements;
@@ -32,6 +27,21 @@ abstract class Stmt {
     }
 
     final List<Stmt> statements;
+  }
+
+  static class Class extends Stmt {
+    Class(Token name, List<Stmt.Function> methods) {
+      this.name = name;
+      this.methods = methods;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitClassStmt(this);
+    }
+
+    final Token name;
+    final List<Stmt.Function> methods;
   }
 
   static class Expression extends Stmt {
@@ -46,7 +56,6 @@ abstract class Stmt {
 
     final Expr expression;
   }
-
   static class Function extends Stmt {
     Function(Token name, List<Token> params, List<Stmt> body) {
       this.name = name;
@@ -63,7 +72,6 @@ abstract class Stmt {
     final List<Token> params;
     final List<Stmt> body;
   }
-
   static class If extends Stmt {
     If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
       this.condition = condition;
@@ -80,7 +88,6 @@ abstract class Stmt {
     final Stmt thenBranch;
     final Stmt elseBranch;
   }
-
   static class Print extends Stmt {
     Print(Expr expression) {
       this.expression = expression;
@@ -93,7 +100,6 @@ abstract class Stmt {
 
     final Expr expression;
   }
-
   static class Return extends Stmt {
     Return(Token keyword, Expr value) {
       this.keyword = keyword;
@@ -108,7 +114,6 @@ abstract class Stmt {
     final Token keyword;
     final Expr value;
   }
-
   static class Var extends Stmt {
     Var(Token name, Expr initializer) {
       this.name = name;
@@ -123,7 +128,6 @@ abstract class Stmt {
     final Token name;
     final Expr initializer;
   }
-
   static class While extends Stmt {
     While(Expr condition, Stmt body) {
       this.condition = condition;
