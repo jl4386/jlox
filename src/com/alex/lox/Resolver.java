b2222b85a5bd51f4a5ce306313337126254ec169
giddens9527 <jl4386@outlook.com>
2022-03-12 10:54:00 +0800

    feat: basic super error handling

--- src/com/alex/lox/Resolver.java
+++ src/com/alex/lox/Resolver.java
@@ -24,7 +24,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum ClassType {
     None,
-    CLASS
+    CLASS,
+    SUBCLASS
   }
 
 
@@ -46,6 +47,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
       Lox.error(stmt.superclass.name, "A class can't inherit from itself.");
     }
     if (stmt.superclass != null) {
+      currentClass = ClassType.SUBCLASS;
       resolve(stmt.superclass);
     }
     if (stmt.superclass != null) {
@@ -185,6 +187,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitSuperExpr(Expr.Super expr) {
+    if (currentClass == ClassType.None) {
+      Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+    } else if (currentClass != ClassType.SUBCLASS) {
+      Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+    }
     resolveLocal(expr, expr.keyword);
     return null;
   }
